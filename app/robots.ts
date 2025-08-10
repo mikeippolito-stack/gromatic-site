77f291bffb5549f3a34955f69906bae85e0b97a0
mikeippolito-stack <mike.ippolito1@gmail.com>
2025-08-10 14:38:04 +1000

    Update robots.ts

--- app/robots.ts
+++ app/robots.ts
@@ -1,1 +1,11 @@
-import { MetadataRoute } from 'next'; export default function robots(): MetadataRoute.Robots{ return{ rules:{userAgent:'*', allow:'/'}, sitemap:'https://www.gromatic.co/sitemap.xml' } }
+import { MetadataRoute } from 'next';
+
+export default function robots(): MetadataRoute.Robots {
+  return {
+    rules: {
+      userAgent: '*',
+      allow: '/',
+    },
+    sitemap: 'https://www.gromatic.co/sitemap.xml',
+  };
+}
