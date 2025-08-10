a4db947ce210cec323a287678f64a5a36bb74756
mikeippolito-stack <mike.ippolito1@gmail.com>
2025-08-10 12:35:57 +1000

    Delete lib directory

--- lib/sanity.image.ts
+++
@@ -1,5 +0,0 @@
-import imageUrlBuilder from '@sanity/image-url';
-import { client } from './sanity.client';
-
-const builder = imageUrlBuilder(client);
-export const urlFor = (src: any) => builder.image(src);
