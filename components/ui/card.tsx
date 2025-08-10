a20baddead29a2e864cc388450274a1a39590c9c
mikeippolito-stack <mike.ippolito1@gmail.com>
2025-08-10 12:57:16 +1000

    Delete components directory

--- components/ui/card.tsx
+++
@@ -1,8 +0,0 @@
-import * as React from 'react';
-type DivProps = React.HTMLAttributes<HTMLDivElement>;
-export function Card({ className='', children, ...props }:{className?:string; children:React.ReactNode} & DivProps){
-  return <div {...props} className={`bg-white border rounded-2xl ${className}`}>{children}</div>;
-}
-export function CardContent({ className='', children, ...props }:{className?:string; children:React.ReactNode} & DivProps){
-  return <div {...props} className={`p-6 ${className}`}>{children}</div>;
-}
