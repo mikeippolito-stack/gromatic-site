bd4235554348e9c32e6251eaad189a9126389bc3
mikeippolito-stack <mike.ippolito1@gmail.com>
2025-08-10 14:15:20 +1000

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,5 +2,5 @@
 module.exports = {
   content: ["./app/**/*.{js,ts,jsx,tsx,mdx}", "./components/**/*.{js,ts,jsx,tsx,mdx}"],
   theme: { extend: {} },
-  plugins: [require('@tailwindcss/typography')],
+  plugins: [],
 };
