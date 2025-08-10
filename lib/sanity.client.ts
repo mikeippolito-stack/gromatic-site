8417528579d17f27ec392fbee70cf5468862edf1
mikeippolito-stack <mike.ippolito1@gmail.com>
2025-08-10 12:57:40 +1000

    Delete lib directory

--- lib/sanity.client.ts
+++
@@ -1,2 +0,0 @@
-import { createClient } from 'next-sanity';
-export const client=createClient({projectId:process.env.NEXT_PUBLIC_SANITY_PROJECT_ID!,dataset:process.env.NEXT_PUBLIC_SANITY_DATASET||'production',apiVersion:process.env.NEXT_PUBLIC_SANITY_API_VERSION||'2025-08-01',useCdn:true});
--- lib/sanity.queries.ts
+++
@@ -1,3 +0,0 @@
-import { groq } from 'next-sanity';
-export const postsQuery=groq`*[_type=='post']|order(date desc){_id,title,'slug':slug.current,excerpt,date,cover}`;
-export const postBySlugQuery=groq`*[_type=='post'&&slug.current==$slug][0]{_id,title,'slug':slug.current,excerpt,date,cover,body}`;
